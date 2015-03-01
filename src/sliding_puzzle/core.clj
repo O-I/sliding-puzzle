83b4d89c0453485d2f83b980eeaf1d0b92abf7bc
Rahul Horé <hore.rahul@gmail.com>
2015-02-28 20:54:49 -0500

    Represents grid as a map instead of as a vector
    I couldn't figure out a better way to bundle the grid's height n (which I name :size) with it's state (called :tiles) given my current inexperience with Clojure. I need this number for many calculations and it doesn't make sense to calculate it over and over since it remains constant for a given grid. As I keep learning, perhaps I'll find a more idiomatic way to encapsulate the size of a grid with its state.

--- src/sliding_puzzle/core.clj
+++ src/sliding_puzzle/core.clj
@@ -1,49 +1,49 @@
 (ns sliding-puzzle.core)
 
 (defn random-grid
-  "Builds a random n x n vector with unique values from
-   0 to n^2 - 1 that represents a sliding puzzle grid"
+  "Builds a map that represents a sliding puzzle grid.
+   :size is the height/width n of the grid and :tiles
+   is an n^2 - 1 random vector representing the grid state."
    [n]
-   (def size n)
-   (shuffle (range 0 (* n n))))
+   (hash-map :size n :tiles (shuffle (range (* n n)))))
 
 (defn blank-at
   "Finds the blank in the grid represented by 0"
   [grid]
-  (.indexOf grid 0))
+  (.indexOf (:tiles grid) 0))
 
 (defn tile-count
   "Returns the number of tiles in grid plus the blank"
   [grid]
-  (count grid))
+  (count (:tiles grid)))
 
 (defn solved?
   "Returns true if grid is in a solved state"
   [grid]
-  (= (conj (vec (range 1 (tile-count grid))) 0) grid))
+  (= (conj (vec (range 1 (tile-count grid))) 0) (:tiles grid)))
 
 (defn blank-at-row
   "Returns the row of the blank tile"
   [grid]
-  (quot (blank-at grid) size))
+  (quot (blank-at grid) (:size grid)))
 
 (defn blank-at-column
   "Returns the column of the blank tile"
   [grid]
-  (rem (blank-at grid) size))
+  (rem (blank-at grid) (:size grid)))
 
 (defn slices
   "Returns a list of tile-count many lists where each
   is the previous one with the head dropped"
   [grid]
-  (map #(drop % grid) (range (tile-count grid))))
+  (map #(drop % (:tiles grid)) (range (tile-count grid))))
 
 (defn inversions
   "Returns the number of inversions in a grid"
   [grid]
   (reduce +
     (map count
-         (map (fn [n] (filter #(< 0 % (nth grid n)) (drop n grid)))
+         (map (fn [n] (filter #(< 0 % (nth (:tiles grid) n)) (drop n (:tiles grid))))
               (range (tile-count grid))))))
 
 (defn -main [] )
--- test/sliding_puzzle/core_test.clj
+++ test/sliding_puzzle/core_test.clj
@@ -5,13 +5,13 @@
 (deftest random-grid-test
   (testing "random-grid"
     (let [puz (random-grid 3)]
-      (is (= 3 size))
-      (is (= 9 (count puz)))
-      (is (= (set (range 0 9)) (set puz))))))
+      (is (= 3 (:size puz)))
+      (is (= 9 (count (:tiles puz))))
+      (is (= (set (range 9)) (set (:tiles puz)))))))
 
 (deftest blank-at-test
   (testing "blank-at"
-    (is (= 6 (blank-at [2 3 1 4 7 5 0 6 8])))))
+    (is (= 6 (blank-at {:tiles [2 3 1 4 7 5 0 6 8]})))))
 
 (deftest tile-count-test
   (testing "tile-count"
@@ -19,23 +19,23 @@
 
 (deftest solved?-test
   (testing "solved?"
-    (is (true?  (solved? [1 2 3 4 5 6 7 8 0])))
-    (is (false? (solved? [1 5 2 3 4 0 7 8 6])))))
+    (is (true?  (solved? {:tiles [1 2 3 4 5 6 7 8 0]})))
+    (is (false? (solved? {:tiles [1 5 2 3 4 0 7 8 6]})))))
 
 (deftest blank-at-row-test
   (testing "blank-at-row"
-    (is (= 2 (blank-at-row [2 3 1 4 7 5 0 6 8])))))
+    (is (= 2 (blank-at-row {:size 3 :tiles [2 3 1 4 7 5 0 6 8]})))))
 
 (deftest blank-at-column-test
   (testing "blank-at-column"
-    (is (= 0 (blank-at-column [2 3 1 4 7 5 0 6 8])))))
+    (is (= 0 (blank-at-column {:size 3 :tiles [2 3 1 4 7 5 0 6 8]})))))
 
 (deftest slices-test
   (testing "slices"
     (is (= '((1 2 3 4) (2 3 4) (3 4) (4))
-           (slices [1 2 3 4])))))
+           (slices {:tiles [1 2 3 4]})))))
 
 (deftest inversions-test
   (testing "inversions"
-    (is (= 8 (inversions [2 6 3 1 0 4 7 8 5])))
-    (is (= 0 (inversions [1 2 3 4 5 6 7 8 0])))))
+    (is (= 8 (inversions {:tiles [2 6 3 1 0 4 7 8 5]})))
+    (is (= 0 (inversions {:tiles [1 2 3 4 5 6 7 8 0]})))))
