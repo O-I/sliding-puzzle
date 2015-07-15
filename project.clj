079a1ba85179036bb42ff937d675af941901fbee
Rahul Horé <hore.rahul@gmail.com>
2015-07-14 22:35:32 -0400

    Updates project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sliding-puzzle "0.1.0-SNAPSHOT"
   :description "Sliding puzzle solver"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/O-I/sliding-puzzle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
