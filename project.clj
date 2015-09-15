05942c7c8d347eb81b95d0a48d32594c8245e1c6
Rahul Horé <hore.rahul@gmail.com>
2015-09-14 23:10:26 -0400

    Updates project.clj
    Try Clojure 1.8.0 alpha

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/O-I/sliding-puzzle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.priority-map "0.0.5"]
                  [com.taoensso/timbre "3.4.0"]]
   :main sliding-puzzle.core)
